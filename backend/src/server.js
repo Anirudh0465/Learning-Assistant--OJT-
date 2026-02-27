98f72831e93ebb736e66477b5e59d550eabc3de7
Anirudh <anirudh0465@gmail.com>
2026-02-27 16:30:35 +0530

    server hosted and mongodb connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import connectDB from "./config/db.js";
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/",(req,res) => {
   res.send("Server is running");
 });
 

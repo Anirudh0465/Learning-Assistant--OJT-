a3f9138fa2ed771b9a4e75d041aa03d0b0d34a86
Anirudh <anirudh0465@gmail.com>
2026-03-03 14:53:11 +0530

    database connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.js";
+import test from "./models/test.js"
 dotenv.config();
 
 connectDB();
@@ -22,4 +22,17 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+
+//testing database
+app.use(express.json());
+app.post('/users', async(req,res)=>{
+  try{
+    const{ name, email} = req.body;
+    const user = await test.create({ name,email });
+  res.status(201).json(user);
+  }catch(error){
+    res.status(500).json({error: error.message});
+  } 
 });
